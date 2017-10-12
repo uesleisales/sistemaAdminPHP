76bdbfe4e65037a0f7b866e44feb735d415b26ed
uesleisales <wesleijt@hotmail.com>
2017-10-12 12:46:40 -0300

    área de administração em desenvolvimento

--- administrativo.php
+++ administrativo.php
@@ -1,8 +1,97 @@
-<?php
-  session_start();
-  include_once("seguranca.php");
-  echo "Bem vindo administrador ".$_SESSION['usuarioNome'];
-?>
 
-<br />
-<a href="sair.php">Sair</a>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Página administrativa">
+    <meta name="author" content="Ueslei sales">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Área administrativa</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/theme.css" rel="stylesheet">
+
+    
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    
+  </head>
+
+  <body>
+
+  	<?php
+  		include_once("menu_admin.php");
+  	?>
+
+    
+
+    <div class="container theme-showcase" role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      
+
+      
+
+      <div class="page-header">
+        <h1>Lista de usuários</h1>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ menu_admin.php
@@ -0,0 +1,16 @@
+<!-- Fixed navbar -->
+<ul class="nav nav-pills justify-content-center">
+	<li class="nav-item">
+	<a class="nav-link active" href="administrativo.php">Home</a>
+	</li>
+	<li class="nav-item">
+	<a class="nav-link" href="listar_usuario.php">Lista de usuários</a>
+	</li>
+	<li class="nav-item">
+	<a class="nav-link" href="cad_usuario.php">Cadastrar</a>
+	</li>
+	<li class="nav-item">
+	<a class="nav-link " href="sair.php">Sair</a>
+	</li>
+</ul>
+<br />
